8278e115133bb64a30d13e25e2b77675f9697216
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 17:47:48 +0700

    first commit

---
+++ bin/inner_function.dart
@@ -0,0 +1,7 @@
+void main(){
+  void sapa(){
+    print('inilah adalah inner function');
+  }
+
+  sapa();
+}
---
+++ test/inner_function_test.dart
@@ -0,0 +1,8 @@
+import 'package:inner_function/inner_function.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
